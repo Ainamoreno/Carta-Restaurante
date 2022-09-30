34813898fe0a4d275129aa8730dcfc68762f8918
Aina <ainamoreno@outlook.com>
2022-09-30 19:48:08 +0200

    Función mostrar y cerrar toast de reserva

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Con esta función se abre el Toast 
+function mostrarToast() {
+    var toast = document.getElementById("toast");
+    toast.className = "mostrar";
+    setTimeout(function(){ toast.className = toast.className.replace("mostrar", ""); }, 7000);
+}
+
+
+// Con esta función se cierra el Toast 
+function cerrarToast() {
+    var toast = document.getElementById("toast");
+    toast.className = "cerrar";
+    toast.className = toast.className.replace("cerrar", "");
+}
