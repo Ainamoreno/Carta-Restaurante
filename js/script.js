fb3c9fa3959fdc2008d67f6ed862ff1147cd130b
Aina <ainamoreno@outlook.com>
2022-10-01 12:59:00 +0200

    Función reservar

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
+const inputDia = document.getElementById('input-dia');
+const inputMes = document.getElementById('input-mes');
+const inputAno = document.getElementById('input-ano');
+const inputPersonas = document.getElementById('input-personas');
+const inputHora = document.getElementById('input-hora');
+
 // Con esta función se abre el Toast 
 function mostrarToast() {
     var toast = document.getElementById("toast");
     toast.className = "mostrar";
-    setTimeout(function(){ toast.className = toast.className.replace("mostrar", ""); }, 7000);
+    setTimeout(function(){ toast.className = toast.className.replace("mostrar", ""); }, 7000); 
+    inputDia.value = 'Elige...';
+    inputMes.value = 'Elige...';
+    inputAno.value = 'Elige...';
+    inputPersonas.value = 'Elige...';
+    inputHora.value = 'Elige...';
 }
-
 
 // Con esta función se cierra el Toast 
 function cerrarToast() {
@@ -13,5 +23,11 @@ function cerrarToast() {
     toast.className = toast.className.replace("cerrar", "");
 }
 
-const inputDia = document.getElementById('input-dia');
-console.log(inputDia)
+function validarFormulario () {
+    console.log(inputDia.value)
+    const esInvalid = [inputDia.value, inputMes.value, inputAno.value, inputPersonas.value, inputHora.value].includes('Elige...');
+    document.getElementById('btn-reservar').disabled = esInvalid;
+}
+
+
+
